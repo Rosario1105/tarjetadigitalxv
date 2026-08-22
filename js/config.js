dd51c67fd49d9bb5779a569d974cd6e973772aa4
Rosario Pittaro <rosario.pittaro1105@gmail.com>
2026-08-22 15:23:15 -0300

    Feat: Creacion de pantalla principal

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const invitationConfig = {
+    name: 'Mili',
+    eventDate: "2026-10-03T22:00:00",
+    venue: "La Serviliana",
+    address: "99Q4+J4, Marapa, Tucumán",
+    mapsUrl: 'https://maps.app.goo.gl/chnnKTqY6ivFXxCZ8',
+    
+}
